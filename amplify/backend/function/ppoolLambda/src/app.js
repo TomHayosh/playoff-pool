4418578b826377bff95358687066fbd1ec708314
Tom Hayosh <thayosh@gmail.com>
2019-12-26 11:13:49 -0600

    Successful foundation - email as username and POST works

--- amplify/backend/function/ppoolLambda/src/app.js
+++ amplify/backend/function/ppoolLambda/src/app.js
@@ -22,7 +22,7 @@ if(process.env.ENV && process.env.ENV !== "NONE") {
   tableName = tableName + '-' + process.env.ENV;
 }
 
-const userIdPresent = false; // TODO: update in case is required to use that definition
+const userIdPresent = true; // TODO: update in case is required to use that definition
 const partitionKeyName = "id";
 const partitionKeyType = "S";
 const sortKeyName = "";
