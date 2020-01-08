24b58af01c0202b456ff5b651c2071da881d5b5d
Tom Hayosh <thayosh@gmail.com>
2020-01-07 18:15:47 -0600

    Showing round 2 results, not allowing round 2 picks yet

--- src/App.js
+++ src/App.js
@@ -136,19 +136,26 @@ class App extends Component {
     }
     const response = await API.get("ppoolApi", "/items/week1/fakeId");
     var tempgrid1 = {...this.state.r1table};
+    var tempgrid2 = {...this.state.r2table};
     var j = 0;
-    var gamestarted = [true, true, true, true];
+    var gamestarted = [true, true, true, true, true, true, true];
     for (var i = 0; i < response.length; i++) {
       const isCurrentUserHack = response[i]['edit-r1g1'] ||
         response[i]['edit-r1g2'] ||
         response[i]['edit-r1g3'] ||
-        response[i]['edit-r1g4'];
+        response[i]['edit-r1g4'] ||
+        response[i]['edit-r2g1'] ||
+        response[i]['edit-r2g2'] ||
+        response[i]['edit-r3g1'];
         if (isCurrentUserHack) {
           gamestarted = [
               !response[i]['edit-r1g1'],
               !response[i]['edit-r1g2'],
               !response[i]['edit-r1g3'],
-              !response[i]['edit-r1g4']
+              !response[i]['edit-r1g4'],
+              !response[i]['edit-r2g1'],
+              !response[i]['edit-r2g2'],
+              !response[i]['edit-r3g1']
           ]
         }
         this.setState({r1started: [...gamestarted]});
@@ -184,12 +191,27 @@ class App extends Component {
             }
           }
         }
-        tempgrid1.rows[j++] = {name: response[i]['fullname'], total: total, r1g1: picks[0], r1g2: picks[1], r1g3: picks[2], r1g4: picks[3]};
+        tempgrid1.rows[j] = {name: response[i]['fullname'], total: total, r1g1: picks[0], r1g2: picks[1], r1g3: picks[2], r1g4: picks[3]};
+        var delta = 4;
+        for (var p = 0; p < 2; p++) {
+          if (response[i]['r2g' + (p+1)] !== undefined) {
+            picks[p] = response[i]['r2g' + (p+1)];
+            if (gamestarted[p+delta]) {
+              total += Math.abs(picks[p] - this.state.r2margins[p]);
+              picks[p] += " (" + (Math.abs(picks[p] - this.state.r2margins[p])) + ")";
+            }
+          }
+        }
+        tempgrid2.rows[j] = {name: response[i]['fullname'], total: total, r2g1: picks[0], r2g2: picks[1]};
+        j++;
       } else if (onmount) {
         if (response[i]['id'] === 'awayTeam') {
           // TODO: Fix hard coded g index. Match on 'at' instead
           for (var g = 1; g <= 4; g++) {
             tempgrid1.columns[g+1].label = response[i]['r1g' + g] + ' ' + tempgrid1.columns[g+1].label;
+          }
+          for (var g = 1; g <= 2; g++) {
+            tempgrid2.columns[g+1].label = response[i]['r2g' + g] + ' ' + tempgrid2.columns[g+1].label;
           }
         } else if (response[i]['id'] === 'homeTeam') {
           for (var g = 1; g <= 4; g++) {
@@ -199,10 +221,18 @@ class App extends Component {
               tempgrid1.columns[g+1].label += " (" + this.state.r1margins[g-1] + ")";
             }
           }
+          for (var g = 1; g <= 2; g++) {
+            tempgrid2.columns[g+1].label = tempgrid2.columns[g+1].label + ' ' + response[i]['r1g' + g];
+            if (gamestarted[g-1]) {
+              // TODO: Make this independent of response row ordering. Away team side doesn't work.
+              tempgrid2.columns[g+1].label += " (" + this.state.r1margins[g-1] + ")";
+            }
+          }
         }
       }
     }
     this.setState({r1table: tempgrid1});
+    this.setState({r2table: tempgrid2});
   }
 
   handleChange = event => {
@@ -262,7 +292,7 @@ class App extends Component {
         {this.state.idFound ? (
           <div>
           <form onSubmit={this.update}>
-              <legend>2020 NFL Wild Card Round</legend>
+              <legend>2020 NFL Divisional Round</legend>
               <MDBContainer>
                   <MDBRow>
                     {!this.state.r1started[0] ?
@@ -315,7 +345,7 @@ class App extends Component {
                 searching={false}
                 data={this.state.r2table}
               />
-              <legend>2020 NFL Wild Card Round</legend>
+              <legend>2020 NFL Divisional Round</legend>
             </div>
           : <span/> }
           <MDBDataTable
