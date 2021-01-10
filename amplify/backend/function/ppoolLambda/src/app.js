a2d283ed85a2150950fa76cf2bf90033be1eb6ec
Tom Hayosh <thayosh@gmail.com>
2021-01-10 17:41:55 -0600

    Added flag indicating if user has joined pool in current year.

--- amplify/backend/function/ppoolLambda/src/app.js
+++ amplify/backend/function/ppoolLambda/src/app.js
@@ -344,7 +344,7 @@ app.get(path + '/object' + hashKeyPath + sortKeyPath, function(req, res) {
       res.statusCode = 500;
       res.json({error: 'Could not load items: ' + err.message});
     } else {
-      if (data.Item) {
+      if (data.Item && data.Item.joined == true) {
         console.log("Found item - test");
         res.json(data.Item);
       } else {
